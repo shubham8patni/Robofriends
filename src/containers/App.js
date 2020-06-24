b88ec802ad41d3f85587f1a984e4519e3353eff6
Shubham Patni <39957304+shubham8patni@users.noreply.github.com>
2020-06-24 17:36:09 +0530

    Robofirends
    First React app

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList.js';
+import SearchBox from'../components/SearchBox.js';
+import {robots} from '../Robots.js';
+import Scroll from '../components/Scroll.js'
+import './App.css';
+import '../index.css';
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+
+		}
+	}
+
+	componentDidMount(){
+		this.setState({robots : robots})
+	}
+
+	onSearchChange = (event) =>{
+		this.setState({searchfield: event.target.value})
+				
+	}
+
+	render (){
+
+		const filteredRobots = this.state.robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+			})
+
+	return(
+		<div className='tc '>
+		<h1 className='f1'>RoboFriends </h1>
+		<SearchBox searchChange={this.onSearchChange} />
+		<Scroll>
+			<CardList robots={filteredRobots} />
+		</Scroll>
+		</div>
+	);
+}
+}
+
+export default App;
